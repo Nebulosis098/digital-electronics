a18a2dff2a8d9936834defbbebcd08fdc2e4f707
Maximiliano Eberhardt <nebulosis098@gmail.com>
2024-04-17 12:33:58 -0300

    Ejercicio2_guia2

---
+++ nor_gate_ej2_gtp2/nor_gate.v
@@ -0,0 +1,22 @@
+module moduleName (
+
+// Seccion de declaracion de puertos i/o (inputs outputs) que tiene el circuito
+    input wire A,
+    input wire B,
+    input wire C,
+    output wire X
+);
+
+// Signal declarations -> Internal variables 
+
+
+//Body: Describe the internal organization of circuit 
+always @() begin
+
+    X <= ~(A | B | C); // PORQUE NO ANDAAAAAA? HDP    
+
+end
+
+
+    
+endmodule 
